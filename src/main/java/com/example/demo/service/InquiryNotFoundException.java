3eb90c6ccfc29972dae16fe350912969ae4662f7
torako-san <100730549+torako-san@users.noreply.github.com>
2022-10-13 14:41:20 +0900

    frist comit

---
+++ src/main/java/com/example/demo/service/InquiryNotFoundException.java
@@ -0,0 +1,32 @@
+package com.example.demo.service;
+
+public class InquiryNotFoundException extends RuntimeException {
+
+//	public InquiryNotFoundException() {
+//		// TODO 自動生成されたコンストラクター・スタブ
+//	}
+	
+	private static final long serialVersionUID = 1L;
+
+	public InquiryNotFoundException(String message) {
+		super(message);
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+//	public InquiryNotFoundException(Throwable cause) {
+//		super(cause);
+//		// TODO 自動生成されたコンストラクター・スタブ
+//	}
+//
+//	public InquiryNotFoundException(String message, Throwable cause) {
+//		super(message, cause);
+//		// TODO 自動生成されたコンストラクター・スタブ
+//	}
+//
+//	public InquiryNotFoundException(String message, Throwable cause, boolean enableSuppression,
+//			boolean writableStackTrace) {
+//		super(message, cause, enableSuppression, writableStackTrace);
+//		// TODO 自動生成されたコンストラクター・スタブ
+//	}
+
+}
